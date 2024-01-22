3969896e7f77c9e456e60ea50c94e87175b2f7fb
reindert <info@codesensei.nl>
2024-01-22 22:06:09 +0100

    After Module 3: Creating a Simple Web Page

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from website.views import welcome, about
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', welcome),
+    path('about', about)
 ]
