ee464032945b964e3b36ba597061b82241997a7e
reindert <info@codesensei.nl>
2024-04-23 17:20:33 +0200

    After Module 6: URLs and Link Building

---
+++ meeting_planner/meetings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from meetings import views
+
+
+urlpatterns = [
+    path('<int:id>', views.detail, name="detail"),
+    path('rooms', views.rooms_list, name="rooms")
+]
